3a481ecf4211537736e8c46b71d41b60c4d73f05
yoma sofwan <yoma@femaledaily.com>
2020-06-03 15:12:22 +0700

    Fetch dta hackernews, show data list story on index page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,34 @@
 import React, { Component } from 'react';
+import fetch from 'isomorphic-fetch';
+import Error from 'next/error';
 
 export default class index extends Component {
+  static async getInitialProps() {
+    let stories;
+    try {
+      const res = await fetch('https://node-hnapi.herokuapp.com/news?page=1');
+      stories = await res.json();
+    } catch (error) {
+      console.log('err: ', error);
+      stories = [];
+    }
+
+    return { stories };
+  }
   render() {
-    return <div>hacker news</div>;
+    const { stories } = this.props;
+    if (stories.length === 0) {
+      return <Error statusCode={503} />;
+    }
+    return (
+      <div>
+        <h1>hacker news</h1>
+        <div>
+          {stories.map((item) => (
+            <h2 key={item.id}>{item.title}</h2>
+          ))}
+        </div>
+      </div>
+    );
   }
 }
