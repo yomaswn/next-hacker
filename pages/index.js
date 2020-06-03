4808647f1290ff524206ace4e28250b83e64dc9a
yoma sofwan <yoma@femaledaily.com>
2020-06-03 18:51:49 +0700

    Add story page, fetch data from api

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,54 @@
 import React, { Component } from 'react';
 import fetch from 'isomorphic-fetch';
 import Error from 'next/error';
+import Link from 'next/link';
 
 import StoryList from '../components/StoryList';
 import Layout from '../components/Layout';
 
 export default class index extends Component {
-  static async getInitialProps() {
+  static async getInitialProps({ req, res, query }) {
     let stories;
+    let page;
     try {
-      const res = await fetch('https://node-hnapi.herokuapp.com/news?page=1');
+      page = Number(query.page) || 1;
+      const res = await fetch(
+        `https://node-hnapi.herokuapp.com/news?page=${page}`
+      );
       stories = await res.json();
     } catch (error) {
       console.log('err: ', error);
       stories = [];
     }
 
-    return { stories };
+    return { stories, page };
   }
   render() {
-    const { stories } = this.props;
+    const { stories, page } = this.props;
     if (stories.length === 0) {
       return <Error statusCode={503} />;
     }
     return (
       <Layout title='Next Hacker' description='Hacker news build with next'>
         <StoryList stories={stories} />
+
+        <footer>
+          <Link href={`/?page=${page + 1}`}>
+            <a>Next Page ({page + 1})</a>
+          </Link>
+        </footer>
+        <style>
+          {`
+            footer {
+              padding: 1em;
+            }
+            footer a {
+              font-weight: bold;
+              color: black;
+              text-decoration: none;
+            }
+          `}
+        </style>
       </Layout>
     );
   }
---
+++ pages/story.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import fetch from 'isomorphic-fetch';
+import Error from 'next/error';
+import Link from 'next/link';
+
+import Layout from '../components/Layout';
+
+export class story extends Component {
+  static async getInitialProps({ req, res, query }) {
+    let storyId = query.id;
+    let story;
+    try {
+      const res = await fetch(
+        `https://node-hnapi.herokuapp.com/item/${storyId}`
+      );
+      story = await res.json();
+    } catch (error) {
+      console.log('err: ', error);
+      story = null;
+    }
+
+    return { story };
+  }
+  render() {
+    const { story } = this.props;
+
+    if (!story) {
+      return <Error statusCode={503} />;
+    }
+    return (
+      <Layout title={story.title}>
+        <main>
+          <h1 className='story-title'>
+            <a href={story.url}>{story.title}</a>
+          </h1>
+          <div className='story-details'>
+            <strong>{story.points} points</strong>
+            <strong>{story.comments_count} comments</strong>
+            <strong>{story.time_ago}</strong>
+          </div>
+        </main>
+
+        <style jsx>
+          {`
+            main {
+              padding: 1em;
+            }
+            .story-title {
+              font-size: 1.2rem;
+              margin: 0;
+              font-weight: 300;
+              padding-bottom: 0.5em;
+            }
+            .story-title a {
+              color: #333;
+              text-decoration: none;
+            }
+            .story-title a:hover {
+              text-decoration: underline;
+            }
+            .story-details {
+              font-size: 0.8rem;
+              padding-bottom: 1em;
+              border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+              margin-bottom: 1em;
+            }
+            .story-details strong {
+              margin-right: 1em;
+            }
+            .story-details a {
+              color: #f60;
+            }
+          `}
+        </style>
+      </Layout>
+    );
+  }
+}
+
+export default story;
